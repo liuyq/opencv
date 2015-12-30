4b76c9c3caec0234651ab02aa7f1848374a42ec0
Yongqin Liu <yongqin.liu@linaro.org>
2015-12-30 12:04:20 +0800

    can compile the OpenCVManager.apk
    but still has problem to integrate the so files
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,4 @@ TOP_LOCAL_PATH:= $(call my-dir)
 include $(TOP_LOCAL_PATH)/modules/core/Android.mk
 include $(TOP_LOCAL_PATH)/modules/java/Android.mk
 include $(TOP_LOCAL_PATH)/samples/android/15-puzzle/Android.mk
+include $(TOP_LOCAL_PATH)/platforms/android/service/engine/Android.mk
---
+++ platforms/android/service/engine/Android.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2011 Jack Palevich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This makefile shows how to build a shared library and an activity that
+# bundles the shared library and calls it using JNI.
+
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := OpenCVManager
+LOCAL_MODULE_TAGS := debug
+LOCAL_STATIC_JAVA_LIBRARIES := OpenCVJavaCommon
+
+include $(BUILD_PACKAGE)
