3a5670d5ae088b1241f70980335c126507aae279
Yongqin Liu <yongqin.liu@linaro.org>
2016-01-17 16:53:03 +0800

    15-puzzle: set to use SDK 11 explicitly
    otherwise it will report the no permission problem
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- samples/android/15-puzzle/Android.mk
+++ samples/android/15-puzzle/Android.mk
@@ -21,6 +21,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := OpenCV15Puzzle
+LOCAL_SDK_VERSION := 11
 LOCAL_MODULE_TAGS := debug
 LOCAL_STATIC_JAVA_LIBRARIES := OpenCVJavaCommon
 include $(BUILD_PACKAGE)
